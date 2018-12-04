211177938459805e82ffbf9b2e63a93c17ffe68b
Mika-Matti Auerkallio <mikamatti.auerkallio@gmail.com>
2018-12-04 11:06:08 +0200

    comment

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ io.on('connection', function(socket)
     console.log('user connected');
     console.log('Connected: %s sockets connected', connections.length);
 
-    socket.username = "Odottaa nimeä";
+    socket.username = "Nimetön";
     users.push(socket.username);
     updateUsernames();
     
