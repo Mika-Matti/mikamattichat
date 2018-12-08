57f0d06bb240e3fd6d1168ea21cf7af8910eda03
Mika-Matti <machinegunmajesty@gmail.com>
2018-12-08 21:40:40 +0200

    comment

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ let app = express();
 let http = require('http').createServer(app); //lisäsin tähän .Server tilalle .createServer herokua varten ota pois jos ei toimi 
 let io = require('socket.io')(http);
 
-let users = []; //käyttäjälista
+//let sanitize = require('validator').sanitize; //putsataan input mahdollisista koodi injectioneista.
+
+let users = {}; //käyttäjälista
 let connections = [];
 let PORT = process.env.PORT || 3000;
 //let inout;
@@ -34,12 +36,15 @@ http.listen(PORT, function()
 io.on('connection', function(socket)
 {
     //on connection
+    //koitetaan kellonaikaa tehdä timestamppeihin socket.emit('datetime', { datetime: new Date().getTime() });
+    
+
     connections.push(socket);
     console.log('user connected');
     console.log('Connected: %s sockets connected', connections.length);
 
-    socket.username = "Nameless One";
-    users.push(socket.username);
+    socket.username = "newUser" + Math.random().toString(36).substr(2, 5);
+    users[socket.username] = socket;
     updateUsernames();
     updateConnections();
     
@@ -49,7 +54,7 @@ io.on('connection', function(socket)
     socket.on('disconnect', function()
     {
     hasLeft();
-    users.splice(users.indexOf(socket.username), 1);
+    delete users[socket.username];
     updateUsernames();    
 
     connections.splice(connections.indexOf(socket), 1);
@@ -60,70 +65,98 @@ io.on('connection', function(socket)
     
     });
 
-    //broadcastataan kopattu viesti takaisin kaikille serveriltä
-    socket.on('chat message', function(data)
+    //viestin lähettäminen ikkunaan
+    socket.on('chat message', function(data, callback)
     {
-        io.emit('chat message', {msg: data, user: socket.username});
-        //toimii console.log('viesti lähetetty takaisin clientsideen');
-        console.log('message:', {user: socket.username, msg: data});
+        var msg = data.trim();
+        if(msg.substr(0,3) === '/w ') //tällä komennolla voi lähettää yksityisviestin
+        {
+            msg = msg.substr(3); //poistetaan viestistä /w
+            var ind = msg.indexOf(' ');
+            if(ind !== -1)
+            {
+                var name = msg.substring(0, ind);
+                var msg = msg.substring(ind + 1);
+                if(name in users)
+                {
+                    users[name].emit('whisper', {msg: msg, user: socket.username}); //lähetetään yksityisviesti mutta users[name ei löydä oikeaa usernamee] muuten toimii
+                    socket.emit('whisper', {msg: msg, user: socket.username});      // lähettää viestin myös itselle ikkunaan eli current socket
+                    console.log('whisper', {user: socket.username, msg: msg, name:name});          //siis index puolellakin toimii mutta users[name] ei toimi
+                }
+                else
+                {
+                    callback('That user is not in the room.');
+                }
+                
+            }
+            else
+            {
+                callback('You cannot send an empty whisper.');
+            }
+            
+        }
+        else //ilman komentoa lähetetään tavallinen viesti kaikille
+        {            
+            io.emit('new message', {msg: msg, user: socket.username});
+            console.log('message:', {user: socket.username, msg: data});
+        }
     });
 
     //nimimerkin asetus toimii nyt
     socket.on('new user', function(data, callback)
     {
-        if(users.indexOf(data) != -1) //jos nimi löytyy jo arraysta
+        if(data in users) //jos nimi löytyy jo 
         {
              callback(false);
              console.log ("nimi -" + data + "- on jo käytössä");
-             console.log ("lista nimistä: " + users);
+             console.log("Lista nimistä: " + Object.keys(users));
         }
         else
         {
             
         
         callback(true);
-        users.splice(users.indexOf(socket.username), 1);
+        delete users[socket.username];
         socket.username = data;
-        users.push(socket.username);
+        users[socket.username] = socket;
         hasJoined();
         updateUsernames();
         updateUsername();
         updateConnections();
 
-        console.log ("username is " + data);
-        console.log ("lista nimistä: " + users);
+        console.log ("username set to " + data);
+        console.log("Lista nimistä: " + Object.keys(users));
         }
 
     });
     //nimenvaihto
-    socket.on('change user', function(newdata, callback)
+    socket.on('change user', function(data, callback)
         {
-            if(users.indexOf(newdata) != -1) //jos nimi löytyy jo arraysta
+            if(data in users) //jos nimi löytyy jo arraysta
             {
                 callback(false);
-                console.log ("nimi -" + newdata + "- on jo käytössä");
-                console.log ("lista nimistä: " + users);
+                console.log ("nimi -" + data + "- on jo käytössä");
+                console.log("Lista nimistä: " + Object.keys(users));
             }
             else
             {
-                callback(true);
+                callback(true);                
                 nameChangestart();
-                users.splice(users.indexOf(socket.username), 1); // tän pitäisi poistaa vanha
-                socket.username = newdata;
-                users.push(socket.username);
+                delete users[socket.username]; // tän pitäisi poistaa vanha
+                socket.username = data;
+                users[socket.username] = socket;
                 updateUsernames();
-                updateUsername();
+                updateUsername();                
                 nameChangeend();
-                //updateConnections();
-
-                console.log("new username is " + newdata);
-                console.log("lista nimistä: " + users);
+                console.log("username changed to " + data);
+                console.log("Lista nimistä: " + Object.keys(users));
             }
         });
 
+
     function updateUsernames()
     {
-        io.sockets.emit('get users', users); //io.sockets.emit broadcastaa KAIKILLE
+        io.sockets.emit('get users', Object.keys(users)); //io.sockets.emit broadcastaa KAIKILLE
     }
 
     function updateUsername()
@@ -134,7 +167,7 @@ io.on('connection', function(socket)
 
     function updateConnections()
     {
-        io.sockets.emit('get connections', users.length)
+        io.sockets.emit('get connections', connections.length)
     }
 
     function hasJoined()
